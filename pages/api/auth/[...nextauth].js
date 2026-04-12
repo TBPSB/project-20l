77899c920a6d4fc8ccbfd257f55bc14fb24c9bfe
Bhanu Pratap Singh Bisht <52692924+TBPSB@users.noreply.github.com>
2026-04-12 14:54:08 +0530

    Refactor NextAuth configuration for better structure

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -20,4 +20,6 @@ export default NextAuth({
   pages: {
     error: "/",
   },
-});
+};
+
+export default NextAuth(authOptions);
